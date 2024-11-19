1e0f27566531a2c310789cf033c3c81abff75b38
jong <jongperono@gmail.com>
2024-11-19 16:45:16 +0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  const names = ['Alice', 'Bob', 'Charlie', 'Diana'];
+  return (
+    <div className="p-10">
+      <h2>List of Names</h2>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
